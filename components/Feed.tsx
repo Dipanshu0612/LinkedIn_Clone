3ff37118a74c6fc34bc189f5803227c2159e3732
Dipanshu Mishra <dipanshu.a.mishra06@gmail.com>
2024-06-06 23:33:39 +0530

    Fixing key error.

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,11 +1,12 @@
 import { IPostDocument } from "@/mongodb/models/posts";
 import Post from "./Post";
+import { Key } from "react";
 
 async function Feed({ posts }: { posts: IPostDocument[] }) {
   return (
     <div className="space-y-2 pb-20">
       {posts?.map((post) => (
-        <Post key={post._id} post={post} />
+        <Post key={post._id as Key} post={post} />
       ))}
     </div>
   );
